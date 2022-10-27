8a583fa9214769479fd4732d48d3bf146315bf42
m_wallet_app <mwallet.apps@gmail.com>
2022-10-27 16:06:03 +0200

    check null or empty in getActiveAndCompletedStats() func

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/StatisticsUtilsTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/StatisticsUtilsTest.kt
@@ -23,6 +23,25 @@ class StatisticsUtilsTest {
     }
 
     @Test
+    fun getActiveAndCompletedStats_both_returnsFortySixty(){
+        // Create an active tasks (the false makes this active)
+        val tasks = listOf<Task>(
+            Task("title", "desc", isCompleted = true),
+            Task("title", "desc", isCompleted = true),
+            Task("title", "desc", isCompleted = false),
+            Task("title", "desc", isCompleted = false),
+            Task("title", "desc", isCompleted = true)
+        )
+
+        // Call our function
+        val result = getActiveAndCompletedStats(tasks)
+
+        // Check the result
+        assertEquals(40f, result.completedTasksPercent)
+        assertEquals(60f, result.activeTasksPercent)
+    }
+
+    @Test
     fun getActiveAndCompletedStats_empty_returnsZeros(){
         // Create an active tasks (the false makes this active)
         val tasks = emptyList<Task>()
@@ -30,8 +49,8 @@ class StatisticsUtilsTest {
         val result = getActiveAndCompletedStats(tasks)
 
         // Check the result
-        assertEquals(0f, result.completedTasksPercent)
-        assertEquals(0f, result.activeTasksPercent)
+        assertEquals(0.0, result.completedTasksPercent)
+        assertEquals(0.0, result.activeTasksPercent)
     }
 
     @Test
